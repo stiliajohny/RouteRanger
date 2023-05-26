3526f83c00c02486718bfd42e8598e1b7b2768bd
John Stilia <stilia.johny@gmail.com>
2023-05-26 23:08:13 +0100

    Add 3D and 2D map view toggle icons

--- src/screens/GeneralScreen/MapViewToggle.js
+++ src/screens/GeneralScreen/MapViewToggle.js
@@ -1,12 +1,28 @@
+// MapViewToggle.js
 import React from 'react';
 import { View, TouchableOpacity } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
-import GeneralScreenStyles from './GeneralScreenStyles';
+import styles from './GeneralScreenStyles';
 
 export default function MapViewToggle({ mapView, setMapView }) {
         return (
-                <View>
-                        {/* Add your JSX content here */}
+                <View style={styles.iconContainer}>
+                        <TouchableOpacity onPress={() => setMapView('3D')}>
+                                <Ionicons
+                                        name="md-globe"
+                                        size={24}
+                                        color={mapView === '3D' ? 'black' : '#ccc'}
+                                        style={styles.icon}
+                                />
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={() => setMapView('2D')}>
+                                <Ionicons
+                                        name="md-map"
+                                        size={24}
+                                        color={mapView === '2D' ? 'black' : '#ccc'}
+                                        style={styles.icon}
+                                />
+                        </TouchableOpacity>
                 </View>
         );
 }

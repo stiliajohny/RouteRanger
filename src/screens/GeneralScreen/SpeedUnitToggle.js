25247538ffe301de2fd46d0abfe56f0ae6f3c24e
John Stilia <stilia.johny@gmail.com>
2023-05-26 23:09:07 +0100

    Refactor GeneralScreen toggle components to use new styles

--- src/screens/GeneralScreen/SpeedUnitToggle.js
+++ src/screens/GeneralScreen/SpeedUnitToggle.js
@@ -1,16 +1,28 @@
+// SpeedUnitToggle.js
 import React from 'react';
 import { View, TouchableOpacity } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
-import GeneralScreenStyles from './GeneralScreenStyles';
+import styles from './GeneralScreenStyles';
 
 export default function SpeedUnitToggle({ isSpeedUnitKMH, setSpeedUnit }) {
-        const toggleSpeedUnit = () => {
-                setSpeedUnit(!isSpeedUnitKMH);
-        };
-
         return (
-                <View>
-                        {/* Add your JSX content here */}
+                <View style={styles.iconContainer}>
+                        <TouchableOpacity onPress={() => setSpeedUnit(true)}>
+                                <Ionicons
+                                        name="md-speedometer"
+                                        size={24}
+                                        color={isSpeedUnitKMH ? 'black' : '#ccc'}
+                                        style={styles.icon}
+                                />
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={() => setSpeedUnit(false)}>
+                                <Ionicons
+                                        name="md-walk"
+                                        size={24}
+                                        color={!isSpeedUnitKMH ? 'black' : '#ccc'}
+                                        style={styles.icon}
+                                />
+                        </TouchableOpacity>
                 </View>
         );
 }
--- src/screens/GeneralScreen/ToggleSwitch.js
+++ src/screens/GeneralScreen/ToggleSwitch.js
@@ -1,32 +1,12 @@
+// ToggleSwitch.js
 import React from 'react';
-import { View, Text, Switch, StyleSheet } from 'react-native';
+import { View, Switch } from 'react-native';
+import styles from './GeneralScreenStyles';
 
-const styles = StyleSheet.create({
-        container: {
-                flexDirection: 'row',
-                justifyContent: 'space-between',
-                alignItems: 'center',
-                paddingHorizontal: 16,
-                paddingVertical: 8,
-                borderBottomWidth: 1,
-                borderBottomColor: '#ccc',
-        },
-        text: {
-                flex: 1,
-        },
-        toggleContainer: {
-                flexDirection: 'row',
-                alignItems: 'center',
-        },
-});
-
-export default function ToggleSwitch({ label, value, onValueChange }) {
+export default function ToggleSwitch({ value, onValueChange }) {
         return (
-                <View style={styles.container}>
-                        <Text style={styles.text}>{label}</Text>
-                        <View style={styles.toggleContainer}>
-                                <Switch value={value} onValueChange={onValueChange} />
-                        </View>
+                <View style={styles.toggleContainer}>
+                        <Switch value={value} onValueChange={onValueChange} />
                 </View>
         );
 }

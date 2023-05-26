56b2a92609ec6fde05d7888cf45d09f90a7595ac
John Stilia <stilia.johny@gmail.com>
2023-05-26 23:07:59 +0100

    Refactor GeneralScreenStyles.js to use concise style names

--- src/screens/GeneralScreen/GeneralScreenStyles.js
+++ src/screens/GeneralScreen/GeneralScreenStyles.js
@@ -1,7 +1,30 @@
+// GeneralScreenStyles.js
 import { StyleSheet } from 'react-native';
 
-const GeneralScreenStyles = StyleSheet.create({
-        // Add your styles here
+const styles = StyleSheet.create({
+        container: {
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+                paddingHorizontal: 16,
+                paddingVertical: 8,
+                borderBottomWidth: 1,
+                borderBottomColor: '#ccc',
+        },
+        text: {
+                flex: 1,
+        },
+        toggleContainer: {
+                flexDirection: 'row',
+                alignItems: 'center',
+        },
+        iconContainer: {
+                flexDirection: 'row',
+                alignItems: 'center',
+        },
+        icon: {
+                marginHorizontal: 4,
+        },
 });
 
-export default GeneralScreenStyles;
+export default styles;

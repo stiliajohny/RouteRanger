7e085b4fb91b563b6cdd16b2b8803c532df5ea3f
John Stilia <stilia.johny@gmail.com>
2023-05-27 12:39:43 +0100

    refactor: refactor code to accomodate logic

---
+++ src/screens/SettingsScreen/MainScreen/speedToggleLogic.js
@@ -0,0 +1,24 @@
+// speedToggleLogic.js
+
+export const SPEED_UNITS = ['mph', 'kph', 'm/s'];
+
+export const convertSpeed = (speed, unit) => {
+        switch (unit) {
+                case 'mph':
+                        return speed * 2.23694;
+                case 'kph':
+                        return speed * 3.6;
+                case 'm/s':
+                        return speed;
+                default:
+                        return speed;
+        }
+};
+
+export const getSpeedUnit = (settings) => {
+        if (settings && settings.isSpeedUnitKMH) {
+                return 'kph';
+        } else {
+                return 'mph';
+        }
+};

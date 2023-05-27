6b9213dbd4d772245d561836599e23c9799def21
John Stilia <stilia.johny@gmail.com>
2023-05-27 15:43:45 +0100

    Add GPS location data to DeveloperScreen

--- src/screens/SettingsScreen/DeveloperScreen/DeveloperScreen.js
+++ src/screens/SettingsScreen/DeveloperScreen/DeveloperScreen.js
@@ -1,12 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text } from 'react-native';
 import * as SecureStore from 'expo-secure-store';
+import * as Location from 'expo-location';
 
 export default function DeveloperScreen() {
         const [settings, setSettings] = useState(null);
+        const [locationData, setLocationData] = useState(null);
+
+        const isoTime = new Date(locationData.timestamp).toISOString();
+
+
 
         useEffect(() => {
                 loadSettings();
+                fetchLocationData();
         }, []);
 
         const loadSettings = async () => {
@@ -21,6 +28,17 @@ export default function DeveloperScreen() {
                 }
         };
 
+        const fetchLocationData = async () => {
+                const { status } = await Location.requestForegroundPermissionsAsync();
+                if (status !== 'granted') {
+                        console.log('Permission to access location was denied');
+                        return;
+                }
+
+                const deviceLocation = await Location.getCurrentPositionAsync({});
+                setLocationData(deviceLocation);
+        };
+
         return (
                 <View>
                         {settings && (
@@ -30,6 +48,15 @@ export default function DeveloperScreen() {
                                         <Text>Keep Map North: {settings.keepMapNorth ? 'Yes' : 'No'}</Text>
                                         <Text>Show Speedometer: {settings.showSpeedometer ? 'Yes' : 'No'}</Text>
                                         <Text>Map View: {settings.mapView}</Text>
+                                        <Text>GPS Speed: {locationData ? locationData.coords.speed : 'N/A'}</Text>
+                                        <Text>GPS Accuracy: {locationData ? locationData.coords.accuracy : 'N/A'}</Text>
+                                        <Text>GPS Altitude: {locationData ? locationData.coords.altitude : 'N/A'}</Text>
+                                        <Text>GPS Altitude Accuracy: {locationData ? locationData.coords.altitudeAccuracy : 'N/A'}</Text>
+                                        <Text>GPS Heading: {locationData ? locationData.coords.heading : 'N/A'}</Text>
+                                        <Text>GPS Latitude: {locationData ? locationData.coords.latitude : 'N/A'}</Text>
+                                        <Text>GPS Longitude: {locationData ? locationData.coords.longitude : 'N/A'}</Text>
+                                        <Text>GPS Timestamp: {locationData ? locationData.timestamp : 'N/A'}</Text>
+                                        <Text>GPS ISO time: {isoTime} </Text>
                                 </View>
                         )}
                 </View>

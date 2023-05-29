6d4be38e1b922f686ffc2f070a89596e76b89711
John Stilia <stilia.johny@gmail.com>
2023-05-29 15:13:08 +0100

    update code

--- src/screens/SettingsScreen/GeneralSettingsScreen/SpeedUnitToggle.js
+++ src/screens/SettingsScreen/GeneralSettingsScreen/SpeedUnitToggle.js
@@ -1,32 +1,28 @@
 import React from 'react';
 import { View, TouchableOpacity, Text } from 'react-native';
+import { FontAwesome5 } from '@expo/vector-icons';
 import GeneralScreenStyles from './GeneralScreenStyles';
 
 export default function SpeedUnitToggle({ isSpeedUnitKMH, setSpeedUnit }) {
         return (
-
                 <View style={GeneralScreenStyles.iconContainer}>
-                        <TouchableOpacity onPress={() => setSpeedUnit(true)}>
-                                <Text
-                                        style={{
-                                                color: isSpeedUnitKMH ? 'black' : '#ccc',
-                                                fontSize: 24,
-                                                ...GeneralScreenStyles.icon
-                                        }}
-                                >
-                                        KPH
-                                </Text>
+                        <TouchableOpacity onPress={() => setSpeedUnit(true)} style={GeneralScreenStyles.iconItem}>
+                                <FontAwesome5
+                                        name={isSpeedUnitKMH ? 'tachometer-alt' : 'tachometer-alt'}
+                                        size={24}
+                                        color={isSpeedUnitKMH ? 'black' : '#ccc'}
+                                        style={GeneralScreenStyles.icon}
+                                />
+                                <Text style={[GeneralScreenStyles.iconText, { color: isSpeedUnitKMH ? 'black' : '#ccc' }]}>KPH</Text>
                         </TouchableOpacity>
-                        <TouchableOpacity onPress={() => setSpeedUnit(false)}>
-                                <Text
-                                        style={{
-                                                color: !isSpeedUnitKMH ? 'black' : '#ccc',
-                                                fontSize: 24,
-                                                ...GeneralScreenStyles.icon
-                                        }}
-                                >
-                                        MPH
-                                </Text>
+                        <TouchableOpacity onPress={() => setSpeedUnit(false)} style={GeneralScreenStyles.iconItem}>
+                                <FontAwesome5
+                                        name={!isSpeedUnitKMH ? 'tachometer-alt' : 'tachometer-alt'}
+                                        size={24}
+                                        color={!isSpeedUnitKMH ? 'black' : '#ccc'}
+                                        style={GeneralScreenStyles.icon}
+                                />
+                                <Text style={[GeneralScreenStyles.iconText, { color: !isSpeedUnitKMH ? 'black' : '#ccc' }]}>MPH</Text>
                         </TouchableOpacity>
                 </View>
         );

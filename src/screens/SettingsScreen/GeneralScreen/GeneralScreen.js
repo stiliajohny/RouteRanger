936b27b7f54529864559cd2f226dcc638ae95f56
John Stilia <stilia.johny@gmail.com>
2023-05-29 02:05:32 +0100

    Improve settings screen UI

--- src/screens/SettingsScreen/GeneralScreen/GeneralScreen.js
+++ src/screens/SettingsScreen/GeneralScreen/GeneralScreen.js
@@ -1,6 +1,5 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { View, Text, TouchableOpacity, Switch, ScrollView } from 'react-native';
-
+import React, { useState, useEffect } from 'react';
+import { View, Text, TouchableOpacity, Switch, ScrollView, KeyboardAvoidingView } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import * as SecureStore from 'expo-secure-store';
 import GeneralScreenStyles from './GeneralScreenStyles';
@@ -9,29 +8,16 @@ import ThemeToggle from './ThemeToggle';
 import MapViewToggle from './MapViewToggle';
 import Slider from '@react-native-community/slider';
 
-const colorOptions = [
-    { color: '#ff0000', label: 'Red' },
-    { color: '#00ff00', label: 'Green' },
-    { color: '#0000ff', label: 'Blue' },
-    { color: '#ffff00', label: 'Yellow' },
-    { color: '#ff00ff', label: 'Magenta' },
-    { color: '#800080', label: 'Purple' },
-    { color: '#00ffff', label: 'Cyan' },
-];
-
 export default function GeneralScreen() {
     const [isSpeedUnitKMH, setSpeedUnit] = useState(true);
     const [theme, setTheme] = useState('dark');
     const [keepMapNorth, setKeepMapNorth] = useState(false);
     const [showSpeedometer, setShowSpeedometer] = useState(true);
     const [mapView, setMapView] = useState('3D');
-    const [defaultMapAltitude, setDefaultMapAltitude] = useState(10);
-    const [mapAltitude, setMapAltitude] = useState(10);
+    const [defaultMapAltitude, setDefaultMapAltitude] = useState(100);
     const [defaultMapPitch, setDefaultMapPitch] = useState(45);
-    const [mapPitch, setMapPitch] = useState(45);
-    const [defaultPolylineColor, setDefaultPolylineColor] = useState(colorOptions[0].color);
+    const [defaultPolylineColor, setDefaultPolylineColor] = useState('#ff0000');
     const [defaultPolylineThickness, setDefaultPolylineThickness] = useState(1);
-    const scrollRef = useRef(null);
 
     useEffect(() => {
         loadSettings();
@@ -100,106 +86,135 @@ export default function GeneralScreen() {
         setTheme(newTheme);
     };
 
-    const handleMapAltitudeChange = (value) => {
-        setDefaultMapAltitude(value);
-        setMapAltitude(value);
-    };
-
-    const handleMapPitchChange = (value) => {
-        setDefaultMapPitch(value);
-        setMapPitch(value);
-    };
-
     const handlePolylineColorChange = (color) => {
         setDefaultPolylineColor(color);
     };
 
+    const colorOptions = [
+        { color: '#ff0000', label: 'Red' },
+        { color: '#00ff00', label: 'Green' },
+        { color: '#0000ff', label: 'Blue' },
+        { color: '#ffff00', label: 'Yellow' },
+        { color: '#ff00ff', label: 'Magenta' },
+        { color: '#00ffff', label: 'Cyan' },
+        { color: '#ff8000', label: 'Orange' },
+    ];
+
+    const getHexColorLabel = (color) => {
+        const index = colorOptions.findIndex((option) => option.color === color);
+        if (index !== -1) {
+            return colorOptions[index].label;
+        }
+        return '';
+    };
+
     return (
-        <ScrollView contentContainerStyle={GeneralScreenStyles.screenContainer} ref={scrollRef}>
-            <TouchableOpacity style={GeneralScreenStyles.card}>
-                <Text style={GeneralScreenStyles.cardText}>Speed</Text>
-                <SpeedUnitToggle isSpeedUnitKMH={isSpeedUnitKMH} setSpeedUnit={setSpeedUnit} />
-            </TouchableOpacity>
-            <TouchableOpacity style={GeneralScreenStyles.card}>
-                <Text style={GeneralScreenStyles.cardText}>Theme</Text>
-                <ThemeToggle theme={theme} toggleTheme={toggleTheme} />
-            </TouchableOpacity>
-            <TouchableOpacity style={GeneralScreenStyles.card}>
-                <Text style={GeneralScreenStyles.cardText}>Keep Map North</Text>
-                <View style={GeneralScreenStyles.iconContainer}>
-                    <Switch value={keepMapNorth} onValueChange={setKeepMapNorth} />
-                </View>
-            </TouchableOpacity>
-            <TouchableOpacity style={GeneralScreenStyles.card}>
-                <Text style={GeneralScreenStyles.cardText}>Show Speedometer</Text>
-                <View style={GeneralScreenStyles.iconContainer}>
-                    <Switch value={showSpeedometer} onValueChange={setShowSpeedometer} />
-                </View>
-            </TouchableOpacity>
-            <TouchableOpacity style={GeneralScreenStyles.card}>
-                <Text style={GeneralScreenStyles.cardText}>Map View</Text>
-                <MapViewToggle mapView={mapView} setMapView={setMapView} />
-            </TouchableOpacity>
-            <TouchableOpacity style={GeneralScreenStyles.card}>
-                <Text style={GeneralScreenStyles.cardText}>Default Map Altitude: {mapAltitude}m</Text>
-                <Slider
-                    style={{ width: '100%', height: 40 }}
-                    minimumValue={10}
-                    maximumValue={100}
-                    step={1}
-                    value={mapAltitude}
-                    onValueChange={handleMapAltitudeChange}
-                />
-            </TouchableOpacity>
-            <TouchableOpacity style={GeneralScreenStyles.card}>
-                <Text style={GeneralScreenStyles.cardText}>Default Map Pitch: {mapPitch}°</Text>
-                <Slider
-                    style={{ width: '100%', height: 40 }}
-                    minimumValue={0}
-                    maximumValue={90}
-                    step={1}
-                    value={mapPitch}
-                    onValueChange={handleMapPitchChange}
-                />
-            </TouchableOpacity>
-            <TouchableOpacity style={GeneralScreenStyles.card}>
-                <Text style={GeneralScreenStyles.cardText}>Default Polyline Color</Text>
-                <View style={GeneralScreenStyles.colorOptionsContainer}>
-                    {colorOptions.map((option) => (
-                        <TouchableOpacity
-                            key={option.color}
-                            style={[
-                                GeneralScreenStyles.colorOption,
-                                { backgroundColor: option.color },
-                            ]}
-                            onPress={() => handlePolylineColorChange(option.color)}
-                        >
-                            {defaultPolylineColor === option.color && (
-                                <Ionicons
-                                    name="md-checkmark"
-                                    size={20}
-                                    color="#fff"
-                                    style={GeneralScreenStyles.checkmarkIcon}
-                                />
-                            )}
-                        </TouchableOpacity>
-                    ))}
-                </View>
-                <Text style={GeneralScreenStyles.cardSubText}>
-                    Selected Color: {colorOptions.find((option) => option.color === defaultPolylineColor)?.label}
-                </Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={GeneralScreenStyles.card}>
-                <Text style={GeneralScreenStyles.cardText}>Default Polyline Thickness: {defaultPolylineThickness}</Text>
-                <Slider
-                    style={{ width: '100%', height: 40 }}
-                    minimumValue={0.5}
-                    maximumValue={5}
-                    step={0.5}
-                    value={defaultPolylineThickness}
-                    onValueChange={setDefaultPolylineThickness}
-                />
-            </TouchableOpacity>
-        </ScrollView>
+        <KeyboardAvoidingView style={GeneralScreenStyles.container} behavior="height">
+            <ScrollView contentContainerStyle={GeneralScreenStyles.scrollContainer}>
+                <TouchableOpacity style={GeneralScreenStyles.card}>
+                    <Text style={GeneralScreenStyles.cardText}>Speed</Text>
+                    <SpeedUnitToggle isSpeedUnitKMH={isSpeedUnitKMH} setSpeedUnit={setSpeedUnit} />
+                </TouchableOpacity>
+
+                <TouchableOpacity style={GeneralScreenStyles.card}>
+                    <Text style={GeneralScreenStyles.cardText}>Theme</Text>
+                    <ThemeToggle theme={theme} toggleTheme={toggleTheme} />
+                </TouchableOpacity>
+
+                <TouchableOpacity style={GeneralScreenStyles.card}>
+                    <Text style={GeneralScreenStyles.cardText}>Keep Map North</Text>
+                    <View style={GeneralScreenStyles.iconContainer}>
+                        <Switch value={keepMapNorth} onValueChange={setKeepMapNorth} />
+                    </View>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={GeneralScreenStyles.card}>
+                    <Text style={GeneralScreenStyles.cardText}>Show Speedometer</Text>
+                    <View style={GeneralScreenStyles.iconContainer}>
+                        <Switch value={showSpeedometer} onValueChange={setShowSpeedometer} />
+                    </View>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={GeneralScreenStyles.card}>
+                    <Text style={GeneralScreenStyles.cardText}>Map View</Text>
+                    <MapViewToggle mapView={mapView} setMapView={setMapView} />
+                </TouchableOpacity>
+
+                <TouchableOpacity style={GeneralScreenStyles.card}>
+                    <Text style={GeneralScreenStyles.cardText}>Default Map Altitude</Text>
+                    <View style={GeneralScreenStyles.sliderContainer}>
+                        <Slider
+                            style={GeneralScreenStyles.slider}
+                            minimumValue={10}
+                            maximumValue={100}
+                            step={1}
+                            value={defaultMapAltitude}
+                            onValueChange={setDefaultMapAltitude}
+                        />
+                        <Text style={GeneralScreenStyles.sliderValue}>{defaultMapAltitude}</Text>
+                    </View>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={GeneralScreenStyles.card}>
+                    <Text style={GeneralScreenStyles.cardText}>Default Map Pitch</Text>
+                    <View style={GeneralScreenStyles.sliderContainer}>
+                        <Slider
+                            style={GeneralScreenStyles.slider}
+                            minimumValue={0}
+                            maximumValue={90}
+                            step={1}
+                            value={defaultMapPitch}
+                            onValueChange={setDefaultMapPitch}
+                        />
+                        <Text style={GeneralScreenStyles.sliderValue}>{defaultMapPitch}°</Text>
+                    </View>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={GeneralScreenStyles.card}>
+                    <Text style={GeneralScreenStyles.cardText}>Default Polyline Color</Text>
+                    <View style={GeneralScreenStyles.colorOptionsContainer}>
+                        {colorOptions.map((option) => (
+                            <TouchableOpacity
+                                key={option.color}
+                                style={[
+                                    GeneralScreenStyles.colorOption,
+                                    { backgroundColor: option.color },
+                                ]}
+                                onPress={() => handlePolylineColorChange(option.color)}
+                            >
+                                <View style={GeneralScreenStyles.colorOptionLabelContainer}>
+                                </View>
+                                {defaultPolylineColor === option.color && (
+                                    <Ionicons
+                                        name="md-checkmark"
+                                        size={20}
+                                        color="#fff"
+                                        style={GeneralScreenStyles.checkmarkIcon}
+                                    />
+                                )}
+                            </TouchableOpacity>
+                        ))}
+                    </View>
+                    <Text style={GeneralScreenStyles.cardSubText}>
+                        Selected Color: {getHexColorLabel(defaultPolylineColor)} - {defaultPolylineColor}
+                    </Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={GeneralScreenStyles.card}>
+                    <Text style={GeneralScreenStyles.cardText}>Default Polyline Thickness</Text>
+                    <View style={GeneralScreenStyles.sliderContainer}>
+                        <Slider
+                            style={GeneralScreenStyles.slider}
+                            minimumValue={0.5}
+                            maximumValue={5}
+                            step={0.5}
+                            value={defaultPolylineThickness}
+                            onValueChange={setDefaultPolylineThickness}
+                        />
+                        <Text style={GeneralScreenStyles.sliderValue}>{defaultPolylineThickness}</Text>
+                    </View>
+                </TouchableOpacity>
+            </ScrollView>
+        </KeyboardAvoidingView>
     );
 }
--- src/screens/SettingsScreen/GeneralScreen/GeneralScreenStyles.js
+++ src/screens/SettingsScreen/GeneralScreen/GeneralScreenStyles.js
@@ -1,53 +1,69 @@
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
-        screenContainer: {
+        container: {
                 flex: 1,
                 backgroundColor: '#f5f5f5',
+                padding: 10,
+        },
+        scrollContainer: {
+                flexGrow: 1,
                 alignItems: 'center',
                 justifyContent: 'center',
-                padding: 5,
         },
         card: {
                 width: '90%',
                 backgroundColor: '#fff',
-                padding: 10, // Adjust the value here
-                marginVertical: 5, // Adjust the value here
+                padding: 10,
+                marginVertical: 10,
                 borderRadius: 15,
                 shadowColor: '#000',
                 shadowOffset: { width: 0, height: 2 },
                 shadowOpacity: 0.25,
-                shadowRadius: 3.84,
-                elevation: 5,
+                shadowRadius: 2.84,
+                elevation: 2,
         },
         cardText: {
                 fontSize: 18,
                 fontWeight: 'bold',
                 marginBottom: 10,
+
         },
-        iconContainer: {
+        sliderContainer: {
                 flexDirection: 'row',
+                alignItems: 'center',
         },
-        icon: {
-                marginRight: 10,
+        slider: {
+                flex: 1,
+                marginHorizontal: 10,
+        },
+        sliderValue: {
+                fontSize: 16,
+                fontWeight: 'bold',
         },
         colorOptionsContainer: {
                 flexDirection: 'row',
-                justifyContent: 'space-between',
+                justifyContent: 'center',
                 marginTop: 10,
         },
         colorOption: {
-                width: 40,
-                height: 40,
-                borderRadius: 20,
-                borderWidth: 2,
-                borderColor: '#ccc',
+                width: 35,
+                height: 35,
+                borderRadius: 15,
                 marginRight: 10,
-                justifyContent: 'center',
-                alignItems: 'center',
         },
         checkmarkIcon: {
-                alignSelf: 'center',
+                position: 'absolute',
+                top: 6,
+                right: 6,
         },
-
+        cardSubText: {
+                marginTop: 5,
+                fontSize: 12,
+                color: '#777',
+        },
+        speedUnitContainer: {
+                flexDirection: 'row',
+                alignItems: 'center',
+        },
 });

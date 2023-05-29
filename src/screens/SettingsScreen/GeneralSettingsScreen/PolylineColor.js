a60d6d77be56b5ad552122bd8676af67031f1310
John Stilia <stilia.johny@gmail.com>
2023-05-29 17:01:26 +0100

    Refactor polyline color selection with color picker

--- src/screens/SettingsScreen/GeneralSettingsScreen/PolylineColor.js
+++ src/screens/SettingsScreen/GeneralSettingsScreen/PolylineColor.js
@@ -1,30 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TouchableOpacity, Text, View } from 'react-native';
 import GeneralScreenStyles from './GeneralScreenStyles';
 import { Ionicons } from '@expo/vector-icons';
-
+import { ColorPicker } from 'react-native-color-picker';
 
 export default function DefaultPolylineColor({
         defaultPolylineColor,
         handlePolylineColorChange,
-        colorOptions,
         getHexColorLabel,
 }) {
-        return (
-                <TouchableOpacity style={GeneralScreenStyles.card}>
-                        <Text style={GeneralScreenStyles.cardText}>Default Polyline Color</Text>
-                        <View style={GeneralScreenStyles.colorOptionsContainer}>
-                                {colorOptions.map((option) => (
-                                        <TouchableOpacity
-                                                key={option.color}
-                                                style={[
-                                                        GeneralScreenStyles.colorOption,
-                                                        { backgroundColor: option.color },
-                                                ]}
-                                                onPress={() => handlePolylineColorChange(option.color)}
-                                        >
+        const [colorPickerVisible, setColorPickerVisible] = useState(false);
 
-                                                {defaultPolylineColor === option.color && (
+        const openColorPicker = () => {
+                setColorPickerVisible(true);
+        };
+
+        const closeColorPicker = () => {
+                setColorPickerVisible(false);
+        };
+
+        const handleColorSelection = (color) => {
+                handlePolylineColorChange(color);
+                closeColorPicker();
+        };
+
+        return (
+                <>
+                        <TouchableOpacity style={GeneralScreenStyles.card} onPress={openColorPicker}>
+                                <Text style={GeneralScreenStyles.cardText}>Default Polyline Color</Text>
+                                <View style={GeneralScreenStyles.colorPickerContainer}>
+                                        <View
+                                                style={[GeneralScreenStyles.colorPickerButton, { backgroundColor: defaultPolylineColor }]}
+                                        >
+                                                {defaultPolylineColor === 'transparent' && (
                                                         <Ionicons
                                                                 name="md-checkmark"
                                                                 size={20}
@@ -32,12 +40,20 @@ export default function DefaultPolylineColor({
                                                                 style={GeneralScreenStyles.checkmarkIcon}
                                                         />
                                                 )}
-                                        </TouchableOpacity>
-                                ))}
-                        </View>
-                        <Text style={GeneralScreenStyles.cardSubText}>
-                                Selected Color: {getHexColorLabel(defaultPolylineColor)} - {defaultPolylineColor}
-                        </Text>
-                </TouchableOpacity>
+                                        </View>
+                                        <Text style={GeneralScreenStyles.hexColorText}>
+                                                {getHexColorLabel(defaultPolylineColor)}
+                                        </Text>
+                                </View>
+                        </TouchableOpacity>
+
+                        {colorPickerVisible && (
+                                <ColorPicker
+                                        onColorSelected={handleColorSelection}
+                                        defaultColor={defaultPolylineColor}
+                                        style={{ flex: 1 }}
+                                />
+                        )}
+                </>
         );
 }
